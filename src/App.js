4e2731cd3c9a0d10938be8c8ccae596c939fbe37
Mateusz Krzyżanowski <mateusz.krzyzanowski@thesoftwarehouse.pl>
2017-03-27 21:05:18 +0200

    [RTGM-1] init environment for project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>test</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
