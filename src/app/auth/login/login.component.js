618c6237f7704a06d801b4a4561de82368181c4e
Mateusz Krzyżanowski <mateusz.krzyzanowski@thesoftwarehouse.pl>
2017-04-11 16:05:08 +0200

    [RTGM-4] Add login form with inputs

--- src/app/auth/login/login.component.js
+++ src/app/auth/login/login.component.js
@@ -1,9 +1,27 @@
 // @flow
 
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export class LoginFormComponent extends Component {
   render() {
-    return <div>Login Form</div>;
+    return (
+      <form>
+        <h1>Restagram</h1>
+        <h2>login</h2>
+
+        <div>
+          <input type="text" name="login" placeholder="Username" />
+          <input type="password" name="password" placeholder="Password" />
+          <Link to="/user/password">Forgot?</Link>
+          <input type="submit" value="login" />
+        </div>
+
+        <div>
+          Don't have an account?
+          <Link to="/auth/register">Sign up</Link>
+        </div>
+      </form>
+    );
   }
 }
